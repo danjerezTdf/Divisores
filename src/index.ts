cbec3a21f36ef318e89525992f6e4fb30098e1ec
danjerezTdf <104039810+danjerezTdf@users.noreply.github.com>
2022-05-27 15:32:34 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,12 @@
+/* Ejercicio: Divisores
+• Implemente un método llamado cantidadDeDivisores que
+reciba un número entero y devuelva la cantidad de divisores
+• Por ejemplo, para el número 16, sus divisores son 1, 2, 4, 8,
+16, por lo que la respuesta debería ser 5
+• Re-utilice el método esMultiplo implementado para el ejercicio
+anterior
+*/
+
 let valorInUno: number = Number(prompt(`Ingrese Valor a verificar:`));
 let resultado: number = 0;
 
